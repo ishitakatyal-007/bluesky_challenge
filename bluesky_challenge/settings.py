3d72501041656a536c3cac4e2a6d6e70a0f176c8
ishitakatyal-007 <ishita.katyal.007@gmail.com>
2021-11-16 14:10:26 -0500

    version-4

--- bluesky_challenge/settings.py
+++ bluesky_challenge/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g6rgi9kdy+n0tt+1ll)r+3gptfi#b^rwse0ag1$!tphj2ii8h%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', "ghg-emissions.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "ghg-emissions.herokuapp.com"]
 
 
 # Application definition
@@ -154,7 +154,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (
