1cca4dda9f798138f4b44933cd03e10eb5dccb96
ishitakatyal-007 <ishita.katyal.007@gmail.com>
2021-11-16 09:46:26 -0500

    version-1

--- emissions_data/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EmissionsDataConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'emissions_data'
--- emissions_data/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import GHGsEmissions
-
-class EmissionSerializer(serializers.ModelSerializer):
-    class Meta:
-        db_table = GHGsEmissions
-        fields = ("all", )
